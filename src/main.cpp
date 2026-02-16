43346bc8b2310d8a11c3d894de3a2be3decd050c
Timofey63 <Timofey63d@yandex.ru>
2026-02-16 22:24:23 +0400

    add sencor aht10

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,29 @@
 #include <Arduino.h>
 #include "DisplayManager.h"
 #include "DisplayData.h"
+#include <Adafruit_AHTX0.h>
 //https://github.com/Timofey63/7Seg_Multiplex
 
 DisplayManager displayManager;
+Adafruit_AHTX0 aht;
 
 unsigned long lastUpdate = 0;
 const unsigned timeUpdateData = 5000;
 
-//for test
-int temperature = 0;
 bool isCelsius = true;
+int setValueDispay();
 
 void setup() 
 {
   Serial.begin(9600);
   displayManager.begin();
+
+  if (!aht.begin()) 
+  {
+    Serial.println("Not found sensor AHT10!");
+    while (1) delay(100);
+  }
+
   Serial.println("Display initialized");
 }
 
@@ -23,18 +31,29 @@ void loop()
 {
   displayManager.update();
   
-  if (millis() - lastUpdate > timeUpdateData) {
+  if (millis() - lastUpdate > timeUpdateData) 
+  {
     lastUpdate = millis();
-    //srand(millis());
-    temperature = (rand() % 90) - 40; //(temperature + 1) % 100;
-    Serial.println(rand());
-    isCelsius = !isCelsius;
-
-    DisplayData newData(temperature, isCelsius);
-    displayManager.setValue(newData);
+    
+    int valueDisplay = setValueDispay();
     
     Serial.print(isCelsius ? "Temperature: " : "Percent: ");
-    Serial.print(temperature);
-    Serial.println(isCelsius ? "C" : "%");
+    Serial.print(valueDisplay);
+    Serial.println(isCelsius ? "°C" : "%");
   }
+}
+
+int setValueDispay()
+{
+  sensors_event_t humidity, temperature;
+  aht.getEvent(&humidity, &temperature);
+    
+  isCelsius = !isCelsius;
+  int valueDisplay = isCelsius ? temperature.temperature :
+    humidity.relative_humidity;
+  DisplayData newData(valueDisplay, isCelsius);
+
+  displayManager.setValue(newData);
+
+  return valueDisplay;
 }
